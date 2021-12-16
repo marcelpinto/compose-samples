327d874a10206784e853a10b0bc6e87f19220874
marcelpinto <marcelpinto@google.com>
2021-12-16 16:00:31 +0100

    Questionnaire phase
    - Change start button to update widget state to display first question
    - Display List with MultiChoice questions with Checkboxes
    - Add Next button → On click opens MainActivity with next question

--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/appwidget/SurveyAppWidget.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/appwidget/SurveyAppWidget.kt
@@ -1,42 +1,74 @@
 package com.example.compose.jetsurvey.appwidget
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.glance.Button
-import androidx.glance.GlanceModifier
-import androidx.glance.LocalContext
-import androidx.glance.action.ActionParameters
-import androidx.glance.action.actionParametersOf
-import androidx.glance.action.actionStartActivity
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.glance.*
+import androidx.glance.action.*
 import androidx.glance.appwidget.*
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.ToggleableStateKey
+import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.lazy.LazyColumn
+import androidx.glance.appwidget.lazy.items
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.unit.ColorProvider
 import androidx.glance.layout.*
+import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.example.compose.jetsurvey.Arguments
 import com.example.compose.jetsurvey.MainActivity
+import com.example.compose.jetsurvey.R
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidget.Companion.indexKey
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidget.Companion.questionIndexKey
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.colorAccentDay
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.colorAccentNight
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.colorBackgroundDay
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.colorBackgroundNight
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.colorTextDayNight
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.subtitleStyle
+import com.example.compose.jetsurvey.appwidget.SurveyAppWidgetTheme.titleStyle
+import com.example.compose.jetsurvey.survey.PossibleAnswer
+import com.example.compose.jetsurvey.survey.Question
 import com.example.compose.jetsurvey.survey.Survey
 import com.example.compose.jetsurvey.survey.SurveyRepository
 
 class SurveyAppWidget(private val survey: Survey) : GlanceAppWidget() {
 
+    companion object {
+        val indexKey = intPreferencesKey(Arguments.CurrentIndex)
+        val questionIndexKey = ActionParameters.Key<String>("questionIndex")
+    }
+
+    override val stateDefinition = PreferencesGlanceStateDefinition
+
     @Composable
     override fun Content() {
+        val currentState = currentState<Preferences>()
+        val currentIndex = currentState[indexKey] ?: -1
         Column(
             modifier = GlanceModifier.fillMaxSize()
                 .padding(24.dp)
                 .appWidgetBackground()
                 .background(
-                    day = SurveyAppWidgetTheme.colorBackgroundDay,
-                    night = SurveyAppWidgetTheme.colorBackgroundNight
+                    day = colorBackgroundDay,
+                    night = colorBackgroundNight
                 )
-                .cornerRadius(16.dp),
+                .cornerRadius(24.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            SurveyHomepage(survey.title)
+            if (currentIndex == -1) {
+                SurveyHomepage(survey.title)
+            } else {
+                SurveyQuestionnaire(survey.questions.first())
+            }
         }
     }
 }
@@ -48,28 +80,157 @@ private fun stringResource(stringId: Int) = LocalContext.current.resources.getSt
 private fun ColumnScope.SurveyHomepage(@StringRes title: Int) {
     Text(
         text = stringResource(title),
-        style = SurveyAppWidgetTheme.titleStyle,
+        style = titleStyle,
         modifier = GlanceModifier.fillMaxWidth().padding(bottom = 32.dp)
     )
-    Button(
+    SurveyButton(
         text = "START QUIZ",
+        onClick = actionRunCallback<SurveyNextAction>(
+            actionParametersOf(indexKey.toParametersKey() to 0)
+        )
+    )
+}
+
+@Composable
+private fun SurveyQuestionnaire(question: Question) {
+    val answer = question.answer as PossibleAnswer.MultipleChoice
+    LazyColumn(modifier = GlanceModifier.fillMaxSize()) {
+        item {
+            SurveyQuestionTitle(question.questionText)
+        }
+        item {
+            Text(
+                text = "Select all that apply.",
+                style = subtitleStyle,
+                modifier = GlanceModifier.fillMaxWidth().padding(vertical = 24.dp)
+            )
+        }
+        items(items = answer.optionsStringRes, itemId = { it.toLong() }) { id ->
+            SurveyCheckbox(id)
+        }
+        item {
+            SurveyButton(
+                text = "Next",
+                onClick = actionStartActivity<MainActivity>(
+                    actionParametersOf(indexKey.toParametersKey() to 1)
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun SurveyQuestionTitle(@StringRes question: Int) {
+    Box(
         modifier = GlanceModifier
             .fillMaxWidth()
+            .height(72.dp)
+            .background(
+                day = colorBackgroundNight.copy(alpha = 0.1f),
+                night = colorBackgroundDay.copy(alpha = 0.1f)
+            )
+            .cornerRadius(12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(question),
+            style = titleStyle,
+            modifier = GlanceModifier.fillMaxWidth().padding(8.dp)
+        )
+    }
+}
+
+@Composable
+private fun SurveyCheckbox(@StringRes id: Int) {
+    val key = "answer-$id"
+    val checked = currentState<Preferences>()[booleanPreferencesKey(key)] ?: false
+    val onCheckAction = actionRunCallback<SurveyAnswerAction>(
+        actionParametersOf(
+            questionIndexKey to key,
+            ToggleableStateKey to !checked
+        )
+    )
+    Box(GlanceModifier.padding(bottom = 12.dp)) {
+        Row(
+            modifier = GlanceModifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(horizontal = 12.dp)
+                .background(
+                    imageProvider = ImageProvider(
+                        if (checked) {
+                            R.drawable.checkbox_outline_checked
+                        } else {
+                            R.drawable.checkbox_outline
+                        }
+                    )
+                )
+                .clickable(onCheckAction),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = stringResource(stringId = id),
+                modifier = GlanceModifier.fillMaxWidth().defaultWeight(),
+                style = TextStyle(color = colorTextDayNight)
+            )
+            CheckBox(
+                modifier = GlanceModifier.wrapContentWidth(),
+                checked = checked,
+                onCheckedChange = onCheckAction,
+                colors = CheckBoxColors(
+                    checkedColor = ColorProvider(
+                        day = colorAccentDay,
+                        night = colorAccentNight
+                    )
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun SurveyButton(text: String, onClick: Action, modifier: GlanceModifier = GlanceModifier) {
+    Button(
+        text = text,
+        modifier = modifier.fillMaxWidth()
             .height(48.dp)
             .background(
-                day = SurveyAppWidgetTheme.colorAccentDay,
-                night = SurveyAppWidgetTheme.colorAccentNight
-            ),
-        onClick = actionStartActivity<MainActivity>(
-            actionParametersOf(
-                ActionParameters.Key<Int>(Arguments.CurrentIndex) to 0
+                day = colorAccentDay,
+                night = colorAccentNight
             )
-        ),
+            .cornerRadius(12.dp),
+        onClick = onClick,
         style = TextStyle(
             color = ColorProvider(day = Color.White, night = Color.Black),
             fontWeight = FontWeight.Bold
         )
     )
+}
+
+class SurveyAnswerAction : ActionCallback {
+    override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
+        SurveyAppWidget(SurveyRepository.getSurvey()).run {
+            updateAppWidgetState<Preferences>(context, glanceId) {
+                it.toMutablePreferences().apply {
+                    val indexKey = parameters[questionIndexKey]!!
+                    this[booleanPreferencesKey(indexKey)] = parameters[ToggleableStateKey] ?: false
+                }
+            }
+            update(context, glanceId)
+        }
+    }
+}
+
+class SurveyNextAction : ActionCallback {
+    override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
+        val widget = SurveyAppWidget(SurveyRepository.getSurvey())
+        widget.updateAppWidgetState<Preferences>(context, glanceId) {
+            it.toMutablePreferences().apply {
+                this[indexKey] = parameters[indexKey.toParametersKey()] ?: 0
+            }
+        }
+        widget.update(context, glanceId)
+    }
 }
 
 class SurveyAppWidgetReceiver : GlanceAppWidgetReceiver() {
--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/appwidget/SurveyAppWidgetTheme.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/appwidget/SurveyAppWidgetTheme.kt
@@ -3,20 +3,28 @@ package com.example.compose.jetsurvey.appwidget
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.glance.appwidget.unit.ColorProvider
+import androidx.glance.text.FontStyle
 import androidx.glance.text.FontWeight
 import androidx.glance.text.TextStyle
 
 object SurveyAppWidgetTheme {
     val colorAccentDay = Color(0xFF8101EF)
-    val colorAccentNight = Color(0xFF9575CD)
+    val colorAccentNight = Color(0xFFBA65F1)
     val colorBackgroundDay = Color.White
     val colorBackgroundNight = Color.Black
     val colorTextDay = Color(0xff232323)
     val colorTextNight = Color(0xFFE0E0E0)
 
+    val colorTextDayNight = ColorProvider(day = colorTextDay, night = colorTextNight)
+
     val titleStyle = TextStyle(
         fontSize = 16.sp,
-        color = ColorProvider(day = colorTextDay, night = colorTextNight),
-        fontWeight = FontWeight.Bold
+        color = colorTextDayNight,
+        fontWeight = FontWeight.Bold,
+    )
+    val subtitleStyle = TextStyle(
+        fontSize = 12.sp,
+        color = ColorProvider(day = colorTextDay, night = Color(0xFFB6B6B6)),
+        fontStyle = FontStyle.Italic
     )
 }
